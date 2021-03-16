ef09db7c38dba8f2058df0105675c7c314c7e66d
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-16 09:47:17 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReplaceStringChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> listChar = Console.ReadLine()
+                   .ToCharArray()
+                   .ToList();
+
+            for (int i = 0; i < listChar.Count - 1 ; i++)
+            {
+                char currChar = listChar[i];
+
+                if (currChar == listChar[i + 1])
+                {
+                    listChar.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Concat(listChar));
+        }
+    }
+}
